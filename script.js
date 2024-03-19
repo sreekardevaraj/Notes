6a161b8abc6f2b89c3766b1ee73d6116fc9f02de
sreekardevaraj <94167223+sreekardevaraj@users.noreply.github.com>
2024-03-19 21:58:09 +0530

    Add files via upload
    Create Notes

---
+++ script.js
@@ -0,0 +1,41 @@
+const notescontainer = document.querySelector(".notes-container");
+const createBtn = document.querySelector(".btn");
+let notes = document.querySelectorAll(".input-box");
+
+function showNotes() 
+{
+    notescontainer.innerHTML = localStorage.getItem("notes");
+}
+showNotes();
+
+function updateStorage() 
+{
+    localStorage.setItem("notes",notescontainer.innerHTML);
+}
+
+createBtn.addEventListener("click", ()=>{
+    let inputBox = document.createElement("p");
+    let img = document.createElement("img");
+    inputBox.className = "input-box";
+    inputBox.setAttribute("contenteditable","true");
+    img.src = "./delete.png";
+    notescontainer.appendChild(inputBox).appendChild(img);
+})
+
+notescontainer.addEventListener("click",function(e){
+    if(e.target.tagName === "IMG")
+    {
+        e.target.parentElement.remove();
+        updateStorage();
+    }
+    else if(e.target.tagName === "P")
+    {
+        notes = document.querySelectorAll(".input-vox");
+        notes.forEach(nt => 
+        {
+            nt.onkeyup = function(){
+                updateStorage();
+            }
+        });
+    }
+})
